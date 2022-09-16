fd94ceff1933032b891a4009cdb5c3661727a2d1
OLUSOLA ADEKUNLE <olusola.adekunle@outlook.com>
2022-09-16 16:04:13 +0100

    configure progressive web app, PWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 
-import { MatButtonModule } from "@angular/material/button";
-import { MatIconModule } from "@angular/material/icon";
-import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -14,7 +14,9 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HealthCheckComponent } from './health-check/health-check.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -22,17 +24,26 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     HomeComponent,
     FetchDataComponent,
     NavMenuComponent,
-    HealthCheckComponent
+    HealthCheckComponent,
   ],
   imports: [
-    BrowserModule, HttpClientModule, RouterModule,
+    BrowserModule,
+    HttpClientModule,
+    RouterModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
     MatIconModule,
-    MatToolbarModule
+    MatToolbarModule,
+
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/assets/data.js
@@ -0,0 +1,13 @@
+const product = {
+  name: "Car",
+  color: "black",
+  make: "Toyota",
+  model: "Camry",
+  properties: [
+    { name: "gear", description: 'automatic' },
+    { name: "wheel", description: 'Alloy' },
+    { name: "light", description: 'Fog' },
+    { name: "wheel", description: 'Alloy' },
+  ]
+
+}
